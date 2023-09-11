0986d8ae396452830b797e10e00a32eba854568e
dan9b6 <dkburton0@gmail.com>
2023-09-11 21:25:13 +0100

    Added Call To Action and fixed bg gradient

---
+++ src/slices/CallToAction/index.tsx
@@ -0,0 +1,50 @@
+import { Content } from "@prismicio/client";
+import {
+  SliceComponentProps,
+  JSXMapSerializer,
+  PrismicRichText,
+} from "@prismicio/react";
+import Heading from "@/components/Heading";
+import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
+
+const components: JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="sm" className="font-semibold mb-4 text-center">
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="text-center text-slate-600 mb-8">{children}</p>
+  ),
+};
+
+/**
+ * Props for `CallToAction`.
+ */
+export type CallToActionProps = SliceComponentProps<Content.CallToActionSlice>;
+
+/**
+ * Component for "CallToAction" Slices.
+ */
+const CallToAction = ({ slice }: CallToActionProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="max-w-4xl m-auto shadow-xl px-4 md:px-12 py-12 grid place-items-center rounded-lg bg-gradient-to-tr from-cyan-50 via-white to-emerald-50">
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={components}
+        />
+        <PrismicRichText field={slice.primary.body} components={components} />
+        <Button field={slice.primary.button_link}>
+          {slice.primary.button_text}
+        </Button>
+      </div>
+    </Bounded>
+  );
+};
+
+export default CallToAction;
